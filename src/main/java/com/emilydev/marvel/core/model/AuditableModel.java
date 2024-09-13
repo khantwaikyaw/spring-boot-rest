347de192eaa0e875239716ca96bd50bc0634dc21
Khant Wai Kyaw <khantwaikyaw11@gmail.com>
2024-09-13 16:24:02 +0700

    refactor: remove mapstruct

---
+++ src/main/java/com/emilydev/marvel/core/model/AuditableModel.java
@@ -0,0 +1,4 @@
+package com.emilydev.marvel.core.model;
+
+public class AuditableModel {
+}
--- src/main/java/com/emilydev/marvel/domain/category/entity/dto/CategoryResponseDto.java
+++ src/main/java/com/emilydev/marvel/domain/category/entity/dto/CategoryResponseDto.java
@@ -1,11 +1,17 @@
 package com.emilydev.marvel.domain.category.entity.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.util.UUID;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class CategoryResponseDto {
     UUID id;
